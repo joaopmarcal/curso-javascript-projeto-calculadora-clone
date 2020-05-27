df490ed1aa969867d7126744fc56628a85b74a7e
joao Paulo <joaopaulozangrande@gmail.com>
2020-05-27 17:37:13 -0300

    manipulate elements with DOM

---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,34 @@
+    class CalcController {
+        constructor() {
+            this._displayCalc = "0";
+            this._currentDate;
+            this.initialize();
+        }
+
+        initialize(){
+            let displayCalcEl = document.querySelector('#display');
+            let dataEl = document.querySelector('#data');
+            let timeEl = document.querySelector('#hora');
+
+            displayCalcEl.innerHTML = "4567";
+            dataEl.innerHTML = "01/05/2020";
+            timeEl.innerHTML = "00:00";
+        }
+
+        get displayCalc(){
+            return this._displayCalc;
+        }
+
+        set displayCalc(valor){
+            this._displayCalc = valor;
+        }
+
+        get currentDate(){
+            return this._currentDate;
+        }
+
+        set currentDate(valor){
+            this._currentDate = valor;
+        }
+
+    }
