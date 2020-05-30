f4fc2cfe880b7813f7e7e47343470e5d6e865ac1
joao Paulo <joaopaulozangrande@gmail.com>
2020-05-30 17:29:04 -0300

    i developed the button equal part one

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -27,6 +27,8 @@
             }, 10000)
             */
 
+            this.setLastNumberToDisplay();
+
         }
 
         addEventListenerAll(element,events, fn){
@@ -41,10 +43,14 @@
 
         clearAll(){
             this._operation = [];
+
+            this.setLastNumberToDisplay();
         }
 
         clearEntry(){
             this._operation.pop();
+
+            this.setLastNumberToDisplay();
         }
 
         getLastOperation(){
@@ -72,11 +78,27 @@
 
         calc(){
 
-            let last = this._operation.pop();
+            let last = '';
+
+            if (this._operation.length > 3){
+                last = this._operation.pop();
+            }
 
             let result = eval(this._operation.join(""));
 
-            this._operation = [result,last];
+            if (last == '%'){
+
+                result /= 100;
+
+                this._operation = [result];
+
+            } else {
+
+                this._operation = [result];
+
+                if (last) this._operation.push(last);
+
+            }
 
             this.setLastNumberToDisplay();
 
@@ -92,6 +114,8 @@
                     break;
                 }
             }
+
+            if (!lastNumber) lastNumber = 0;
 
             this.displayCalc = lastNumber;
 
@@ -154,7 +178,7 @@
                     this.addOperation('.')
                     break;
                 case 'igual':
-
+                    this.calc();
                     break;
                 case '0':
                 case '1':
