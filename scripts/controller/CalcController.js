bd005ebf178f86c15df752fb4d3500f05ce29892
joao Paulo <joaopaulozangrande@gmail.com>
2020-05-30 17:00:55 -0300

    using eval to validate the numbers

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,6 +1,7 @@
     class CalcController {
         constructor() {
 
+            this._operation = [];
             this._locale = 'pt-BR';
             this._displayCalcEl = document.querySelector('#display');
             this._dateEl = document.querySelector('#data');
@@ -38,13 +39,138 @@
 
         }
 
+        clearAll(){
+            this._operation = [];
+        }
+
+        clearEntry(){
+            this._operation.pop();
+        }
+
+        getLastOperation(){
+            return this._operation[this._operation.length-1];
+        }
+
+        setLastOperation(value){
+            this._operation[this._operation.length - 1] = value;
+        }
+
+        isOperator(value){
+            return (['+','-','*','%','/'].indexOf(value) > -1);
+        }
+
+        pushOperation(value){
+
+            this._operation.push(value);
+
+            if (this._operation.length > 3){
+
+                this.calc();
+            }
+
+        }
+
+        calc(){
+
+            let last = this._operation.pop();
+
+            let result = eval(this._operation.join(""));
+
+            this._operation = [result,last];
+
+        }
+
+        setLastNumberToDisplay(){
+
+
+
+        }
+
+        addOperation(value){
+
+            if (isNaN(this.getLastOperation())){
+                if (this.isOperator(value)){
+                    this.setLastOperation(value);
+                } else if(isNaN(value)){
+                    console.log('Outra coisa',value);
+                } else {
+                    this.pushOperation(value);
+                }
+            } else {
+                if(this.isOperator(value)){
+                    this.pushOperation(value);
+                } else {
+                    let newValue = this.getLastOperation().toString() + value.toString();
+                    this.setLastOperation(parseInt(newValue));
+
+                    this.setLastNumberToDisplay();
+                }
+
+            }
+
+        }
+
+        setError(){
+            this.displayCalc = "Error"
+        }
+
+        execBtn(value){
+            switch (value) {
+                case 'ac':
+                    this.clearAll();
+                    break;
+                case 'ce':
+                    this.clearEntry();
+                    break;
+                case 'soma':
+                    this.addOperation('+');
+                    break;
+                case 'subtracao':
+                    this.addOperation('-');
+                    break;
+                case 'divisao':
+                    this.addOperation('/');
+                    break;
+                case 'multiplicacao':
+                    this.addOperation('*');
+                    break;
+                case 'porcento':
+                    this.addOperation('%');
+                    break;
+                case 'ponto':
+                    this.addOperation('.')
+                    break;
+                case 'igual':
+
+                    break;
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    this.addOperation(parseInt(value));
+                    break;
+                default:
+                    this.setError();
+                    break;
+            }
+
+        }
+
         initButtonsEvents(){
             let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
             buttons.forEach((btn, index)=>{
                 this.addEventListenerAll(btn, 'click drag', e=>{
 
-                    console.log(btn.className.baseVal.replace('btn-',''));
+                    let textBtn = btn.className.baseVal.replace('btn-','');
+
+                    this.execBtn(textBtn);
 
                 });
 
