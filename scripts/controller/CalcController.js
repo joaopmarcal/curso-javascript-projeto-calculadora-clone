3333484506127c1d1835fef33dffbedeab01bdeb
joao Paulo <joaopaulozangrande@gmail.com>
2020-06-02 19:06:39 -0300

    i make the copy and paste function

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -15,6 +15,34 @@
 
         }
 
+        pasteFromClipBoard(){
+
+            document.addEventListener('paste', e=>{
+
+               let text = e.clipboardData.getData('Text');
+
+               this.displayCalc = parseFloat(text);
+
+            });
+
+        }
+
+        copyToClipboard(){
+
+            let input = document.createElement('input');
+
+            input.value = this.displayCalc;
+
+            document.body.appendChild(input);
+
+            input.select();
+
+            document.execCommand("Copy");
+
+            input.remove();
+
+        }
+
         initialize(){
 
             this.setDisplayDateTime();
@@ -31,6 +59,7 @@
             */
 
             this.setLastNumberToDisplay();
+            this.pasteFromClipBoard();
 
         }
 
@@ -70,6 +99,9 @@
                     case '8':
                     case '9':
                         this.addOperation(parseInt(e.key));
+                        break;
+                    case 'c':
+                        if (e.ctrlKey) this.copyToClipboard();
                         break;
                 }
 
